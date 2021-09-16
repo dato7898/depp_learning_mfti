7baeadda5944579e52686bd1dc892e3dbd466048
david <davidd7598@gmail.com>
2021-09-16 12:03:46 +0600

    lesson 5

--- numpy и лин алг/run_length_encoding.py
+++ numpy и лин алг/run_length_encoding.py
@@ -3,7 +3,7 @@ import itertools
 
 
 def encode(a):
-    return tuple(np.array(z) for z in list(zip(*[(x, len(list(y))) for x, y in itertools.groupby(a)])))
+    return tuple(np.array(z) for z in zip(*[(x, len(list(y))) for x, y in itertools.groupby(a)]))
 
 
 print(encode(np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])))
